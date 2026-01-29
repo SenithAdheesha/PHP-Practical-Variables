a4c2c3c6675f7bb4f74b436f6a34e1c32e5e3df4
Senith Adheesha <senithadeesha7@gmail.com>
2026-01-29 23:07:03 +0530

    Update test.php

--- Variables/test.php
+++ Variables/test.php
@@ -4,10 +4,26 @@
 $a = 5;
 $b = &$a;   // $b is a reference to $a
 
+
 $b = 10;    // changing $b also changes $a
 
 echo $a;    // Output: 10
 echo "<br>";
 echo $b;    // Output: 10
 
+
+//cinstant variable and scope 
+define("sName","Senith");
+$x = "Adheesha";
+$y = 3;
+
+function f1(){
+global $x,$y;
+ echo $x;
+ echo "<br>";
+ echo $y;
+ }
+ 
+ f1();
 ?>
+
